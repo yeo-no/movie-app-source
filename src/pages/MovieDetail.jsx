d9c3320327d486218b6672eedc0358187b62abdf
yeo-no <107870503+yeo-no@users.noreply.github.com>
2025-01-24 12:44:47 +0900

    Add files via upload

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import { useLocation, useParams } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { IMG_BASE_URL } from '../components/Movie';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function MovieDetail() {
   const { title } = useParams();
   const {state } = useLocation();
-  console.log(title);
-  console.log(state);
+  const navigate = useNavigate();
   return (
     <div className="page-container">
-      <div className=""
-          style={{display: "flex"}}>
-        <img src={IMG_BASE_URL + state.poster_path} alt="영화 포스터 이미지"
-            style={{width: "350px", height: "450px"}} />
-        <div className=""
-            style={{fontSize:"32px", color:"white"}}>
-          {title}
+      {/* 화살표 아이콘 */}
+      <FontAwesomeIcon
+        icon={faArrowLeft}
+        className="back-arrow"
+        onClick={() => navigate(-1)} // 뒤로 가기 기능
+      />
+      <div className="movie-detail-container">
+        <img className="movie-detail-img" src={IMG_BASE_URL + state.poster_path} alt="영화 포스터 이미지"/>
+        <div className="movie-detail-contents">
+          <div className="movie-detail-title">
+            {title}
+          </div>
+          <div className="movie-detail-overview">
+              {state.overview}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Movie from '../components/Movie';
 import { dummy } from '../movieDummy';
+import axios from 'axios';
+
+
+
 export default function Movies() {
+  //비구조화 할당, tk
+  const [movies, setMovies] = useState([]);
+  // 데이터 호출
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+        const response = await axios.get('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', {
+          headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${MOVIE_API_KEY}`
+          }
+        });
+        setMovies(response.data.results); // API 응답 데이터 저장
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []); // 컴포넌트 마운트 시 1회 실행
 
   return (
     <div>
       <div className="movies-container">
-        {dummy.results.map((item)=>{
+        {movies && movies.length > 0?
+          (movies.map((item)=>{
             return(
               <Movie
                 key={item.id}
                 title={item.title}
+                overview={item.overview}
                 poster_path={item.poster_path}
                 vote_average={item.vote_average.toFixed(1)+'점'}
               />
             );
-        })}
+          }))
+          :
+          ( <p>Loading...</p>)
+       }
       </div>
     </div>
   );
